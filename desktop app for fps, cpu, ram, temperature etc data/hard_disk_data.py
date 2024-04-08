02a19176131ee9ac3eb43036523f5aa84106892f
forhadisrafil <forhadtuhin5088@gmail.com>
2024-04-08 06:14:57 +0600

    hard disk done

--- desktop app for fps, cpu, ram, temperature etc data/hard_disk_data.py
+++ desktop app for fps, cpu, ram, temperature etc data/hard_disk_data.py
@@ -21,8 +21,10 @@ def show_disk_data(page: ft.Page):
     page.window_resizable = False
     page.update()
 
-    d_disk_data = int(psutil.disk_usage('D:\\').percent)
     c_disk_data = int(psutil.disk_usage('C:\\').percent)
+    d_disk_data = int(psutil.disk_usage('E:\\').percent)
+    e_disk_data = int(psutil.disk_usage('F:\\').percent)
+    f_disk_data = int(psutil.disk_usage('G:\\').percent)
 
     disk_percent_value = Text('', size=20, width=ft.FontWeight.BOLD, )
     page.add(
@@ -30,27 +32,96 @@ def show_disk_data(page: ft.Page):
             controls=[
                 Text("Storage", size=50, width=ft.FontWeight.BOLD, text_align='CENTER'),
                 Row(controls=[
-                    ft.Stack(
-                        [
-                            pg(c_disk_data * 0.01),
-                            ft.Container(
-                                content=Text(c_disk_data, size=20, width=ft.FontWeight.BOLD),
-                                alignment=ft.alignment.center,
+                    Column(
+                        controls=[
+                            ft.Stack(
+                                [
+                                    pg(c_disk_data * 0.01),
+                                    ft.Container(
+                                        content=Text(c_disk_data, size=20, width=ft.FontWeight.BOLD),
+                                        alignment=ft.alignment.center,
 
+                                    ),
+                                ],
+                                width=150,
+                                height=150
                             ),
-                            ft.Container(
-                                content=Text("Disk", size=20, width=ft.FontWeight.BOLD),
-                                alignment=ft.alignment.bottom_center,
-                            ),
+                            Text("Drive: C", size=20, width=ft.FontWeight.BOLD),
                         ],
-                        width=150,
-                        height=150
+                        horizontal_alignment=ft.CrossAxisAlignment.CENTER
                     ),
+                    Column(
+                        controls=[
+                            ft.Stack(
+                                [
+                                    pg(d_disk_data * 0.01),
+                                    ft.Container(
+                                        content=Text(d_disk_data, size=20, width=ft.FontWeight.BOLD),
+                                        alignment=ft.alignment.center,
+
+                                    ),
+                                ],
+                                width=150,
+                                height=150
+                            ),
+                            Text("Drive: D", size=20, width=ft.FontWeight.BOLD),
+                        ],
+                        horizontal_alignment=ft.CrossAxisAlignment.CENTER
+                    ),
+                    Column(
+                        controls=[
+                            ft.Stack(
+                                [
+                                    pg(e_disk_data * 0.01),
+                                    ft.Container(
+                                        content=Text(e_disk_data, size=20, width=ft.FontWeight.BOLD),
+                                        alignment=ft.alignment.center,
+
+                                    ),
+                                ],
+                                width=150,
+                                height=150
+                            ),
+                            Text("Drive: E", size=20, width=ft.FontWeight.BOLD),
+                        ],
+                        horizontal_alignment=ft.CrossAxisAlignment.CENTER
+                    ),
+                    Column(
+                        controls=[
+                            ft.Stack(
+                                [
+                                    pg(f_disk_data * 0.01),
+                                    ft.Container(
+                                        content=Text(f_disk_data, size=20, width=ft.FontWeight.BOLD),
+                                        alignment=ft.alignment.center,
+
+                                    ),
+                                ],
+                                width=150,
+                                height=150
+                            ),
+                            Text("Drive: F", size=20, width=ft.FontWeight.BOLD),
+                        ],
+                        horizontal_alignment=ft.CrossAxisAlignment.CENTER
+                    ),
+
                 ],
                     spacing=30,
-                    vertical_alignment=ft.MainAxisAlignment.SPACE_EVENLY,
+                    # vertical_alignment=ft.CrossAxisAlignment.SPACE_EVENLY,
                     alignment=ft.MainAxisAlignment.CENTER
+                ),
+                Row(
+                    controls=[
+                        ft.CircleAvatar(bgcolor="#96DED1", radius=5),
+                        Text('Free Space', size=10, width=ft.FontWeight.BOLD, color="#96DED1"),
 
+                        ft.CircleAvatar(bgcolor="#FF5733", radius=5),
+                        Text('Usage', size=10, width=ft.FontWeight.BOLD, color="#FF5733"),
+
+                    ],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    vertical_alignment=ft.CrossAxisAlignment.CENTER,
+                    spacing=5,
                 ),
 
             ],
@@ -60,14 +131,6 @@ def show_disk_data(page: ft.Page):
 
         )
     )
-    # while True:
-    #     disk_data = int(psutil.disk_usage('D:\\').percent)
-    #     print(psutil.disk_partitions(all=True))
-    #     print(psutil.disk_usage('D:\\'))
-    #     time.sleep(1)
-    #     pr.value = disk_data * 0.01
-    #     disk_percent_value.value = disk_data
-    #     page.update()
     page.update()
 
 
