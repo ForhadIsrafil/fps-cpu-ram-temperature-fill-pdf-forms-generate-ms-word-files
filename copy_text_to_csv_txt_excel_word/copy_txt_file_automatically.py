daa73040a24da03599c21e15d52f18b9f26b8245
forhadisrafil <forhadtuhin5088@gmail.com>
2024-04-21 07:43:20 +0600

    new updates

---
+++ copy_text_to_csv_txt_excel_word/copy_txt_file_automatically.py
@@ -0,0 +1,29 @@
+from ahk import AHK
+from ahk.directives import NoTrayIcon
+
+import time
+
+ahk = AHK(directives=[NoTrayIcon])
+ahk.set_clipboard("")
+
+
+def copy_txt_file(value):
+    with open("data.txt", 'a', encoding='utf-8') as f:
+        f.write(value + '\n')
+
+
+old_d = ''
+
+while True:
+    d = ahk.get_clipboard()
+    if d != None:
+        if d.strip() != "":
+            if old_d != d:
+                print(d)
+                # with open("data.txt", 'a', encoding='utf-8') as f:
+                #     f.write(d + '\n')
+                copy_txt_file(d)
+                old_d = d
+    time.sleep(0.5)
+
+
